05d64ffab0ec41f149db1dbbe52f4a9b122cd3a6
Steve Godin <steve.god@gmail.com>
2017-03-06 14:56:28 -0800

    Seed questions with faker.js
    Run seeds with:
    
      sequelize db:seed:all

--- seeders/20170306222854-questions-seeder.js
+++ seeders/20170306222854-questions-seeder.js
@@ -1,18 +1,29 @@
 'use strict';
 
+// load faker package
+const faker = require('faker');
+
 module.exports = {
   up: function (queryInterface, Sequelize) {
     // when seeding a model, make sure to also include
     // the createdAt & updatedAt fields
     // you can assign it a new Date() as value
-    const questions = [
-      {
-        title: 'John Doe',
-        content: 'test',
+
+    // Array.from is method to create arrays from various array-like objects
+    // and iterable things
+    // it also be used as follows to create an array of determinate size:
+    // - first argument an object {length: <array-size>}
+    // - second argument a callback whose return value will be used for
+    // array values (similar to mapping over the array)
+    const questions = Array.from(
+      {length: 20},
+      (value, index) => ({
+        title: faker.lorem.words(3),
+        content: faker.lorem.paragraphs(2),
         createdAt: new Date(),
         updatedAt: new Date()
-      }
-    ];
+      })
+    )
     // .bulkInsert first argument is the name of the table
     // we want to use
     // its second argument is an array of objects where each object
