1cef4e8d805764652f4975605e81d2eb55f66d21
Steve Godin <steve.god@gmail.com>
2017-03-07 13:59:14 -0800

    Create answers model and associations
    Ran the following command:
    
      sequelize model:create --name Answer --attributes content:text,QuestionId:integer

---
+++ models/answer.js
@@ -0,0 +1,15 @@
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  const Answer = sequelize.define('Answer', {
+    content: DataTypes.TEXT,
+    QuestionId: DataTypes.INTEGER
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+        Answer.belongsTo(models.Question);
+      }
+    }
+  });
+  return Answer;
+};
