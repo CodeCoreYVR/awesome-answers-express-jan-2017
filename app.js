2aab7ca8984b3ab247fd0152229364fe959f9928
Steve Godin <steve.god@gmail.com>
2017-03-07 14:43:49 -0800

    Install method-override middleware
    Ran the following command:
    
      yarn add method-override

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 
 const index = require('./routes/index');
 const users = require('./routes/users');
@@ -26,6 +27,26 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 // 👇 it can also be used to parse form-data
 app.use(bodyParser.urlencoded({ extended: false }));
+
+// This middleware will check the HTTP headers for the keys: X-HTTP-Method,
+// X-HTTP-Method-Override & X-Method-Override. It'll use the value of whichever
+// key it finds last as the HTTP Verb
+app.use(methodOverride('X-HTTP-Method'));          // Microsoft
+app.use(methodOverride('X-HTTP-Method-Override')); // Google/GData
+app.use(methodOverride('X-Method-Override'));      // IBM
+
+// This function for methodOverride will get the HTTP Verb for the request
+// from the req.body (or, form params). It will look for a key of _method and grab
+// its related value as the HTTP Verb.
+app.use(methodOverride(function (req, res) {
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    // look in urlencoded POST bodies and delete it
+    var method = req.body._method
+    delete req.body._method
+    return method
+  }
+}))
+
 app.use(cookieParser());
 // 👇 will transpile our scss files into css files
 app.use(require('node-sass-middleware')({
